c3fe3f8a94e5e03be55b50db1162643451ae705d
Noborita9 <joaquinbonora9@gmail.com>
2022-06-23 20:56:55 -0300

    segments init

---
+++ powerline/my_segments/custom/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="pomodoro",
+    version="0.0.1",
+    author="Noborita",
+    author_email="joaquinbonora9@gmail.com",
+    description="My Pomodoro.",
+    long_description="A long description for the " + \
+        "package that includes the custom segments.",
+    long_description_content_type="text/plain",
+    packages=setuptools.find_packages(),
+)
